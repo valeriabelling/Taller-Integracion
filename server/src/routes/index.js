631f2f23942d11480ddeaea5e0cbf457925c2d41
valeriabelling <valeriabellingueri@gmail.com>
2019-11-06 19:01:58 -0300

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -9,24 +9,10 @@ const {
   deleteHerr
 } = require("../controllers/herramientas.controller");
 
-const {
-  getHerr,
-  getHerrById,
-  createHerr,
-  updateHerr,
-  deleteHerr
-} = require("../controllers/usuario.controller");
-
 router.get("/herramienta", getHerr);
 router.get("/herramienta/:id", getHerrById);
 router.post("/nueva-herramienta", createHerr);
 router.put("/herramienta/:id", updateHerr);
 router.delete("/herramienta/:id", deleteHerr);
-
-router.get("/usuario", getHerr);
-router.get("/usuario/:id", getHerrById);
-router.post("/nuevo-usuario", createHerr);
-router.put("/usuario/:id", updateHerr);
-router.delete("/usuario/:id", deleteHerr);
 
 module.exports = router;
