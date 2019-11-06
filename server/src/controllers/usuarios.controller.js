efe4cf9aa16943e9fa6fea5c64a0d9e611175b14
JulioRupp <53881252+JulioRupp@users.noreply.github.com>
2019-11-06 17:18:28 -0300

    Abm usuario

--- server/src/controllers/usuarios.controller.js
+++ server/src/controllers/usuarios.controller.js
@@ -1,63 +1,83 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "127.0.0.1",
-    database: "taller",
-    password: "38387446",
-    port: '5432',
+  user: "postgres",
+  host: "127.0.0.1",
+  database: "taller",
+  password: "38387446",
+  port: "5432"
 });
 
-const getHerr = async(req, res) => {
-    const response = await pool.query('SELECT * FROM usuario ORDER BY idusuario ASC');
-    res.status(200).json(response.rows);
+const getHerr = async (req, res) => {
+  const response = await pool.query(
+    "SELECT * FROM usuario ORDER BY idusuario ASC"
+  );
+  res.status(200).json(response.rows);
 };
 
-const getHerrById = async(req, res) => {
-    const idUsuario = parseInt(req.params.idUsuario);
-    const response = await pool.query('SELECT * FROM usuario WHERE idherramienta = $1', [idUsuario]);
-    res.json(response.rows);
+const getHerrById = async (req, res) => {
+  const idUsuario = parseInt(req.params.idUsuario);
+  const response = await pool.query(
+    "SELECT * FROM usuario WHERE idherramienta = $1",
+    [idUsuario]
+  );
+  res.json(response.rows);
 };
 
-const createHerr = async(req, res) => {
-    const { nombre, apellido, correoElectronico, telefono, estado, registrado } = req.body;
-    const response = await pool.query('INSERT INTO usuario (nombre, apellido, correoElectronico, telefono, estado, registrado) VALUES ($1, $2, $3, $4, $5, $6)', [nombre, apellido, correoElectronico, telefono, estado, registrado]);
-    res.json({
-        message: 'Usuario Added successfully',
-        body: {
-            user: { nombre, apellido, correoElectronico, telefono, estado, registrado }
-        }
-    })
-};
-
-const updateHerr = async(req, res) => {
-    const idUsuario = parseInt(req.params.idUsuario);
-    const { correoElectronico, telefono} = req.body;
-
-    const response = await pool.query('UPDATE herramienta SET correoElectronico = $1, telefono = $2 WHERE idusuario = $3', [
+const createHerr = async (req, res) => {
+  const {
+    nombre,
+    apellido,
+    correoElectronico,
+    telefono,
+    estado,
+    registrado
+  } = req.body;
+  const response = await pool.query(
+    "INSERT INTO usuario (nombre, apellido, correoElectronico, telefono, estado, registrado) VALUES ($1, $2, $3, $4, $5, $6)",
+    [nombre, apellido, correoElectronico, telefono, estado, registrado]
+  );
+  res.json({
+    message: "Usuario Added successfully",
+    body: {
+      user: {
+        nombre,
+        apellido,
         correoElectronico,
         telefono,
-        idUsuario
-    ]);
-    res.json('User Updated Successfully');
+        estado,
+        registrado
+      }
+    }
+  });
 };
 
-const deleteHerr = async(req, res) => {
-    const idUsuario = parseInt(req.params.idUsuario);
-    
-    
-        const response = await pool.query('UPDATE usuario SET estado = $1 WHERE idUsuario = $2', [
-            estado,
-            idUsuario
-        ]);
-    
-    res.json(`User ${idUsuario} deleted Successfully`);
+const updateHerr = async (req, res) => {
+  const idUsuario = parseInt(req.params.idUsuario);
+  const { correoElectronico, telefono } = req.body;
+
+  const response = await pool.query(
+    "UPDATE herramienta SET correoElectronico = $1, telefono = $2 WHERE idusuario = $3",
+    [correoElectronico, telefono, idUsuario]
+  );
+  res.json("User Updated Successfully");
+};
+
+const deleteHerr = async (req, res) => {
+  const idUsuario = parseInt(req.params.idUsuario);
+
+  const response = await pool.query(
+    "UPDATE usuario SET estado = $1 WHERE idUsuario = $2",
+    [estado, idUsuario]
+  );
+
+  res.json(`User ${idUsuario} deleted Successfully`);
 };
 
 module.exports = {
-    getHerr,
-    getHerrById,
-    createHerr,
-    updateHerr,
-    deleteHerr
+  getHerr,
+  getHerrById,
+  createHerr,
+  updateHerr,
+  deleteHerr
 };
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,20 +1,32 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
-const { getHerr, getHerrById, createHerr, updateHerr, deleteHerr } = require('../controllers/herramientas.controller');
+const {
+  getHerr,
+  getHerrById,
+  createHerr,
+  updateHerr,
+  deleteHerr
+} = require("../controllers/herramientas.controller");
 
-const { getHerr, getHerrById, createHerr, updateHerr, deleteHerr } = require('../controllers/usuario.controller');
+const {
+  getHerr,
+  getHerrById,
+  createHerr,
+  updateHerr,
+  deleteHerr
+} = require("../controllers/usuario.controller");
 
-router.get('/herramienta', getHerr);
-router.get('/herramienta/:id', getHerrById);
-router.post('/nueva-herramienta', createHerr);
-router.put('/herramienta/:id', updateHerr)
-router.delete('/herramienta/:id', deleteHerr);
+router.get("/herramienta", getHerr);
+router.get("/herramienta/:id", getHerrById);
+router.post("/nueva-herramienta", createHerr);
+router.put("/herramienta/:id", updateHerr);
+router.delete("/herramienta/:id", deleteHerr);
 
-router.get('/usuario', getHerr);
-router.get('/usuario/:id', getHerrById);
-router.post('/nuevo-usuario', createHerr);
-router.put('/usuario/:id', updateHerr)
-router.delete('/usuario/:id', deleteHerr);
+router.get("/usuario", getHerr);
+router.get("/usuario/:id", getHerrById);
+router.post("/nuevo-usuario", createHerr);
+router.put("/usuario/:id", updateHerr);
+router.delete("/usuario/:id", deleteHerr);
 
 module.exports = router;
